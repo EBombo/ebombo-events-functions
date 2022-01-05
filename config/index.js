55a8bb783030eb9ccfac91d802b50ba32fa90385
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2022-01-05 14:52:24 -0500

    test v24 env

--- config/index.js
+++ config/index.js
@@ -1,20 +1,20 @@
 const admin = require("firebase-admin");
 const url = require("url");
 
-console.log(process.env.FIREBASE_TOKEN);
-console.log(process.env.NEXT_PUBLIC_CONFIG);
+console.log("FIREBASE_TOKEN", process.env.FIREBASE_TOKEN);
+console.log("NEXT_PUBLIC_CONFIG", process.env.NEXT_PUBLIC_CONFIG);
 
 if (process.env.NEXT_PUBLIC_CONFIG == undefined) {
-  throw new Error(`process.env.NEXT_PUBLIC_CONFIG is undefined. Value: ${process.env.NEXT_PUBLIC_CONFIG}. Process.env${JSON.stringify(process.env)}`);
+    throw new Error(`process.env.NEXT_PUBLIC_CONFIG is undefined. Value: ${process.env.NEXT_PUBLIC_CONFIG}. Process.env${JSON.stringify(process.env)}`);
 }
 
 const config = JSON.parse(process.env.NEXT_PUBLIC_CONFIG);
 
 process.env.NODE_ENV === "production"
-  ? admin.initializeApp()
-  : admin.initializeApp({
-      credential: admin.credential.cert(config.serviceAccountEventsDev),
-      databaseURL: config.firebase.databaseURL,
+    ? admin.initializeApp()
+    : admin.initializeApp({
+        credential: admin.credential.cert(config.serviceAccountEventsDev),
+        databaseURL: config.firebase.databaseURL,
     });
 
 const adminFirestore = admin.firestore;
@@ -24,19 +24,19 @@ const currentEnvironment = process.env.NODE_ENV;
 const version = "0.0.1";
 
 try {
-  firestore.settings({ ignoreUndefinedProperties: true });
+    firestore.settings({ignoreUndefinedProperties: true});
 } catch (error) {
-  console.error("ignoreUndefinedProperties", error);
+    console.error("ignoreUndefinedProperties", error);
 }
 
 const hostname = (req) => url.parse(req.headers.origin).hostname;
 
 module.exports = {
-  adminFirestore,
-  currentEnvironment,
-  firestore,
-  hostname,
-  auth,
-  config,
-  version,
+    adminFirestore,
+    currentEnvironment,
+    firestore,
+    hostname,
+    auth,
+    config,
+    version,
 };
